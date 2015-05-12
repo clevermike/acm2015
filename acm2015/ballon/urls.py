1e1eaa3fe3eaa948d4e05869f3ba10a7f7c7af6e
clevermike <clevermike@163.com>
2015-05-12 12:33:21 +0800

    acm2015

--- acm2015/ballon/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding :utf-8
-#author :dongchen
-
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('ballon.views',
-    url(r'^/?$', 'login'),
-)
